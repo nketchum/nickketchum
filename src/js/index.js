0ef02beb28e381425fb7428aeab88fe70d1de9c3
Nicholas Ketchum <nketchum@gmail.com>
2017-02-26 16:26:01 -0500

    test modal content

--- src/js/index.js
+++ src/js/index.js
@@ -13,10 +13,34 @@ window.onload = function() {
     app.innerHTML = index;
   }
 
+  function onModalOpen(e) {
+    var target = e.target.getAttribute('href').substring(1);
+    var contents;
+    if (target == 'modal_cinders') {
+      contents =
+      '<p><img src="/images/full-coi-01.png"></p>' +
+      '<p>Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Donec id elit non mi porta gravida at eget metus. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Vestibulum id ligula porta felis euismod semper.</p>' +
+      '<p>Nulla vitae elit libero, a pharetra augue. Maecenas faucibus mollis interdum. Vestibulum id ligula porta felis euismod semper. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla vitae elit libero, a pharetra augue.<p>' +
+      '<p>Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Etiam porta sem malesuada magna mollis euismod. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.<p>' +
+      '<p>Aenean lacinia bibendum nulla sed consectetur. Maecenas faucibus mollis interdum. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.<p>' +
+      '<p>Curabitur blandit tempus porttitor. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Sed posuere consectetur est at lobortis. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed posuere consectetur est at lobortis.<p>';
+    }
+    var extNode = document.getElementById('modal_cinders');
+    extNode.innerHTML = contents;
+  }
+
+  function onModalClose() {
+    var extNode = document.getElementById('modal_cinders');
+    extNode.innerHTML = '';
+  }
+
   function loadScripts() {
     var sticky = new Sticky('nav');
     var lazy = new LazyLoad();
-    var modal = new Modal.default();
+    var modal = new Modal.default({
+      onBeforeOpen: onModalOpen,
+      onClose: onModalClose
+    });
   }
 
   loadIndex();
