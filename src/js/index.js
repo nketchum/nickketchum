a9fb4d330f4d3a20f7f1196b5f09fba8e6eade1b
Nicholas Ketchum <nketchum@gmail.com>
2017-03-04 15:14:24 -0500

    new styles, svgs, and icons

--- src/js/index.js
+++ src/js/index.js
@@ -1,60 +1,44 @@
 'use strict';
 
 window.onload = function() {
-
-  function loadIndex() {
-    const app = document.getElementById('app');
-
-    var ejs = {
-      clientele: require('../ejs/clientele.ejs'),
-      projects:  require('../ejs/projects.ejs'),
-      personal:  require('../ejs/personal.ejs'),
-      skills:    require('../ejs/skills.ejs'),
-      modal:     require('../ejs/modal.ejs')
-    }
-
-    var content = ejs.clientele() + ejs.projects() + ejs.personal() + ejs.skills() + ejs.modal();
-    app.innerHTML = content;
-  }
-
-  function onModalOpen(e) {
-    var modal;
-    var content;
-    var link_id = e.target.getAttribute('href').substring(1);
-
-    // Object key, ejs filename, and link id MUST be identical.
-    var modals = {
-      modal_cinders:    require('../ejs/modals/modal_cinders.ejs'),
-      modal_fpd:        require('../ejs/modals/modal_fpd.ejs'),
-      modal_coi:        require('../ejs/modals/modal_coi.ejs'),
-      modal_rogers:     require('../ejs/modals/modal_rogers.ejs'),
-      modal_colorvid:   require('../ejs/modals/modal_colorvid.ejs'),
-      modal_telemundo:  require('../ejs/modals/modal_telemundo.ejs'),
-      modal_nkwebdev:   require('../ejs/modals/modal_nkwebdev.ejs'),
-      modal_mitravel:   require('../ejs/modals/modal_mitravel.ejs')
-    };
-
-    var modal = document.getElementById(link_id);
-    content = modals[link_id]();
-    modal.innerHTML = content;
-  }
-
-  function onModalClose() {
-    var modal = document.getElementById('modal_cinders');
-    modal.innerHTML = '';
-  }
-
-  function loadScripts() {
-    var sticky = new Sticky('nav');
-    var lazy = new LazyLoad();
-    var modal = new Modal.default({
-      clickOutside: false,
-      onBeforeOpen: onModalOpen,
-      onClose: onModalClose
-    });
-  }
-
-  loadIndex();
-  loadScripts();
-
+  init();
+  scripts();
 };
+
+function init() {
+  var ejs = {
+    clientele: require('../ejs/clientele.ejs'),
+    projects:  require('../ejs/projects.ejs'),
+    personal:  require('../ejs/personal.ejs'),
+    skills:    require('../ejs/skills.ejs'),
+    modal:     require('../ejs/modal.ejs')
+  }
+  var app = document.getElementById('app');
+  app.innerHTML = ejs.clientele() + ejs.projects() + ejs.personal() + ejs.skills() + ejs.modal();
+}
+
+function scripts() {
+  var lazy = new LazyLoad();
+  var sticky = new Sticky('nav');
+  var modal = new Modal.default({
+    clickOutside: false,
+    onBeforeOpen: onBeforeOpen
+  });
+}
+
+function onBeforeOpen(e) {
+  // Object key, link id, and modal div id MUST be identical.
+  var ejs = {
+    modal_cinders:    require('../ejs/modals/modal_cinders.ejs'),
+    modal_fpd:        require('../ejs/modals/modal_fpd.ejs'),
+    modal_coi:        require('../ejs/modals/modal_coi.ejs'),
+    modal_rogers:     require('../ejs/modals/modal_rogers.ejs'),
+    modal_colorvid:   require('../ejs/modals/modal_colorvid.ejs'),
+    modal_telemundo:  require('../ejs/modals/modal_telemundo.ejs'),
+    modal_nkwebdev:   require('../ejs/modals/modal_nkwebdev.ejs'),
+    modal_mitravel:   require('../ejs/modals/modal_mitravel.ejs')
+  };
+  var id = e.target.getAttribute('href').substring(1);
+  var modal = document.getElementById(id);
+  modal.innerHTML = ejs[id]();
+}
